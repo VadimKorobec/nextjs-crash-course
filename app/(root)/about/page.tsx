a68cc0d83bd0fb01576ac79e75299f024cf2d3f1
Vadim <_s_e_t__@ukr.net>
2025-06-03 17:25:25 +0300

    add loading in an about folder

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,5 @@
-const AboutPage = () => {
-  throw new Error("NOT IMPLEMENTET");
+const AboutPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <section>
       <h1 className=" text-3xl">About Page</h1>
